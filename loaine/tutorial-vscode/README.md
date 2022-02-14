5f123281b4e3800f6d52196f0d4dfdb99d0975ec
William Santos <williamdsw@outlook.com>
2022-02-14 16:59:07 -0300

    :ok_hand: Adding Loaine projects

---
+++ loaine/tutorial-vscode/README.md
@@ -0,0 +1,4 @@
+# maven-vscode-tutorial
+
+Projects based on this tutorial:
+https://www.youtube.com/watch?v=23rN0oDdOKg
---
+++ loaine/tutorial-vscode/spring-vscode/src/test/java/com/williamdsw/springvscode/SpringVsCodeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.williamdsw.springvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVsCodeApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
