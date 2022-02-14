24d4324e782795f3a405b1c15a477bc5020d999a
William Santos <williamdsw@outlook.com>
2022-02-14 18:23:10 -0300

    :ok_hand: Adding Baeldung projects

---
+++ baeldung/java-ee-cdi/README.md
@@ -0,0 +1,3 @@
+# An Introduction to CDI (Contexts and Dependency Injection) in Java
+
+Project based on this tutorial: https://www.baeldung.com/java-ee-cdi
---
+++ baeldung/java-ee-cdi/src/main/java/com/williamdsw/services/ImageFileEditor.java
@@ -0,0 +1,13 @@
+package com.williamdsw.services;
+
+public interface ImageFileEditor {
+
+	public String openFile(String fileName);
+
+	public String editFile(String fileName);
+
+	public String writeFile(String fileName);
+
+	public String saveFile(String fileName);
+
+}
